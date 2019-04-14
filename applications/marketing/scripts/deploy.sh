65011c385ae4f8679a010e0ff001f19b57dff292
Devin Brown <devin.g.brown@gmail.com>
2019-04-14 06:55:58 -0600

    misc deploy config/notes

--- applications/marketing/scripts/deploy.sh
+++ applications/marketing/scripts/deploy.sh
@@ -2,6 +2,9 @@
 
 set -efux
 
+# 1. https://github.com/HashNuke/heroku-buildpack-elixir.git
+# 2. https://github.com/gjaldon/heroku-buildpack-phoenix-static.git
+
 pushd ../../
   git subtree push --prefix applications/marketing heroku master
 popd
